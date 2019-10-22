1d12d14e0f4dd8406fb36b83fcf3823bf18c21ba
Sreenadh TC <kesav.tc8@gmail.com>
2019-10-22 13:05:24 +0530

    Expose outputs for RDS

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,8 @@ output "kubeconfig" {
 output "persistence_ids" {
   value = module.infra.persistence_ids
 }
+
+output "this_db_instance_endpoint" {
+  description = "The connection endpoint"
+  value       = "${module.infra.this_db_instance_endpoint}"
+}
