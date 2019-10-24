b5c33332c4c13ab4ace1790bf1827eccea9af51d
Yuva <iffyuva@users.noreply.github.com>
2019-10-24 11:08:45 +0530

    Revert "RDS support"

--- outputs.tf
+++ outputs.tf
@@ -9,8 +9,3 @@ output "kubeconfig" {
 output "persistence_ids" {
   value = module.infra.persistence_ids
 }
-
-output "this_db_instance_endpoint" {
-  description = "The connection endpoint"
-  value       = "${module.infra.this_db_instance_endpoint}"
-}
